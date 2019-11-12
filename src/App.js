084b68c6eab7d66943b954424f4105f2c8ecdc06
tandreig <48963092+tandreig2020@users.noreply.github.com>
2019-11-12 18:54:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,27 @@
 import React from 'react';
 // import ReactDom from 'react-dom'
-import { Icon, Input, Layout, Typography, Empty, Menu, } from 'antd'
+import { Layout, Icon, } from 'antd';
+import Header from './Shared/Header/Header';
+import { Home } from './Shared/Pages/Home/Home'
 import 'antd/dist/antd.css';
 import './App.css';
 
 
 
-const { Header, Content, Footer, } = Layout;
-const { Title } = Typography;
-const { Search } = Input;
-
-
-
-
+const { Content, Footer } = Layout;
 
 function App() {
-
-
-
-
-
-
   return (
 
-    <Layout className="layout">
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header />
 
-      <Header>
-        <div className="logo" />
-        <Title>Movie List App</Title>
-      </Header>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['1']}
-        style={{ lineHeight: '64px' }}>
-        <Menu.Item key="1">Lista Filme</Menu.Item>
-        <Menu.Item key="2">Adauga Film</Menu.Item>
-      </Menu>
-
-
-
-      <Search placeholder="Search movie" onSearch={value => console.log(value)} enterButton />
       <Content style={{ padding: '0 50px' }}>
+        <Home />
+       
 
-
-        <div style={{ background: '#fff', padding: 24, minHeight: 600 }}><Empty></Empty></div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}> <Icon type="copyright" /> 2019 Created by Andrei </Footer>
-
+      <Footer style={{ textAlign: 'center' }}> <Icon type="copyright" /> 2019 Created by TAG </Footer>
     </Layout>
 
 
